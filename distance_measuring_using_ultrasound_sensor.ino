9e88e6c73af62bd4db45f9f7085cd97055013ee8
Engr. Imran Nazir <154506940+Engr-imran7@users.noreply.github.com>
2024-03-05 19:44:43 +0500

    Add files via upload

---
+++ distance_measuring_using_ultrasound_sensor.ino
@@ -0,0 +1,46 @@
+
+#include<Mouse.h>
+
+int led =13;
+const int trigpin =8;
+
+const int echopin =5;
+
+long duration;
+int distance ;
+
+void setup()
+{
+  pinMode(trigpin, OUTPUT);
+
+  pinMode(echopin, INPUT);
+
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin ,HIGH);
+  distance =duration*0.034/2;
+if (distance >=50)
+{
+  digitalWrite(led, HIGH);
+  delay(500);
+}
+else 
+{
+  digitalWrite(led, LOW);
+}
+  Serial.print("          ");
+  Serial.println(distance);
+
+}
+
+
+
